8bedaed28788daaa416000b29d9ebfe3112e1b43
Trevor Suter <Trevorsuter@icloud.com>
2021-01-14 16:42:27 -0700

    created key gen, date conversion, and character set methods with tests, unfinished encrypt and test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,32 @@
+require 'date'
 class Enigma
+
+  def character_set
+    ("a".."z").to_a << " "
+  end
+
+  def key_gen
+    generated_key = String.new
+    until generated_key.length == 5
+      generated_key << rand(0..9).to_s
+    end
+    generated_key
+  end
+
+  def date_conversion
+    year = (Date.today).year.to_s
+    year.slice!(0,2)
+    month_day = Date.today.to_s
+    month_day.delete!('-')
+    month_day.slice!(0,4)
+    date = String.new(month_day + year)
+  end
+
+  def encrypt(message, key = key_gen, date = date_conversion)
+    encryption = Hash.new
+    encryption[:message] = message
+    encryption[:key] = key
+    encryption[:date] = date
+    encryption
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,4 +13,31 @@ class TestEnigma < MiniTest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_character_set
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @enigma.character_set
+  end
+
+  def test_key_gen  
+    assert_equal 5, @enigma.key_gen.length
+  end
+
+  def test_date_conversion
+    Date.stubs(:today).returns(Date.new(2001, 11, 11))
+    assert_equal "111101", @enigma.date_conversion
+  end
+
+  def test_encryption
+    @enigma.stubs(:key_gen).returns("12345")
+    Date.stubs(:today).returns(Date.new(2001, 11, 11))
+    expected = {message: "hello world",
+                key: "02715",
+                date: "040895"}
+    expected2 = {message: "hello world",
+                  key: "12345",
+                  date: "111101"}
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+    assert_equal expected2, @enigma.encrypt("hello world")
+  end
 end
