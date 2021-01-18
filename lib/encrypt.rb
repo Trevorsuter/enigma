c3144c708e6d5103470d4b024030d4881ca25f29
Trevor Suter <Trevorsuter@icloud.com>
2021-01-18 11:52:03 -0700

    successful encrypt runner file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,1 +1,7 @@
 require './lib/enigma'
+enigma = Enigma.new
+message = File.open(ARGV[0].to_s)
+encrypted = File.open(ARGV[1].to_s, 'w')
+encryption = enigma.encrypt(message.read)
+encrypted.write(encryption[:encryption])
+puts "Created '#{ARGV[1]}' with the key #{encryption[:key]} and date #{encryption[:date]}"
